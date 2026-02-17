0e3d878962f29b40b89e73fec9a4d0cf68c12a68
shk0224 <shuvo.du333@gmail.com>
2026-02-16 20:20:17 -0500

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shuvo Dutta",
+    author_email="Shuvo.du333@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
